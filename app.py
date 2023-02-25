447a3c66316c28c94b77fa8052cd7d3da2482638
ananyadaniel <ananyadaniels@gmail.com>
2023-02-26 02:06:42 +0530

    Added comments on how to git after learning something new weee

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ from website import create_app
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
+
+
+#git init
+#git add filename   this is for one file
+#git add .          this is for all the things
