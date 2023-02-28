b4885c9a6d646eda83368b2f83b7259b32458e20
ananyadaniel <ananyadaniels@gmail.com>
2023-02-28 12:05:26 +0530

    completed video 2

--- app.py
+++ app.py
@@ -7,4 +7,14 @@ if __name__ == "__main__":
 
 #git init
 #git add filename   this is for one file
-#git add .          this is for all the things
+#git add .          this is for adding all the things to commit later
+#git status
+#git log
+#git commit -m <a message>
+
+#git remote add origin <github url>
+#git push origin main 
+
+#email: admin@gmail.com
+#password: admin12345
+#username: admin
