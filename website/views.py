625a6332652accd55006d3c16c82e8de483ad77a
ananyadaniel <ananyadaniels@gmail.com>
2023-03-03 00:51:04 +0530

    video 3 ended

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from nis import cat
 from turtle import pos
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required,current_user 
-from .models import Post
+from .models import Post,User
 from website import db
 
 
@@ -41,10 +41,22 @@ def delete_post(id):
     if not post:
         flash("Post does not exist.", category='error')
     elif current_user.id != post.id:
-        flash('You do not have permission to delete the post.')
+        flash('You do not have permission to delete the post.', category='error')
     else:
         db.session.delete(post)
         db.session.commit()
         flash('Post deleted', category='success')
 
     return redirect(url_for('views.home'))
+
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user=User.query.filter_by(username=username).first()
+
+    if not user:
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.home'))
+
+    posts = Post.query.filter_by(author=user.id).all()
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
